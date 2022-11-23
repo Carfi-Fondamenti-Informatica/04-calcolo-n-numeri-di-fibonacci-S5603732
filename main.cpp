ee6245551b03891ec214afe6647d77c8a8023e3a
S5603732 <118397147+S5603732@users.noreply.github.com>
2022-11-23 12:10:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+   int a = 1, b = a, n, c, i=0;
+    cin >> n;
+    if (n>=2) {
+    cout << a << endl;
+    cout << a << endl;
+        while (i < (n - 2)) {
+            i++;
+            c = a + b;
+            cout << c << endl;
+            b = a;
+            a = c;
+        }
+    } else {
+        cout << "errore";
+    }
    return 0;
 }
